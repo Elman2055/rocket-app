188f0bfe4110f0b9390b78dd6c2c039282d9f005
Elman <200522elmanok>
2024-08-11 16:11:50 +0500

    create: choice app background block

--- src/components/mainPage/MainPage.jsx
+++ src/components/mainPage/MainPage.jsx
@@ -1,4 +1,5 @@
 import RunningLine from "../ui/runningLine/RunningLine";
+import bancking from "../../../public/banckingApp.png";
 import "./MainPage.css";
 
 const MainPage = () => {
@@ -12,6 +13,44 @@ const MainPage = () => {
         </div>
       </div>
       <RunningLine />
+      <div className="mainPageContainer">
+        <div className="mainText">
+          <h2>Lorem ipsum dolor sit amet consectetur</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur. Quis ridiculus fringilla vel
+            sem nisi nunc fermentum risus. Posuere id in imperdiet odio sagittis
+            eget est. Arcu tellus diam a morbi. Ut etiam non ornare lorem
+            consectetur ipsum.
+          </p>
+        </div>
+        <p className="choiceTitle">Отличный выбор для всех</p>
+        <div className="choiceApp">
+          <div className="definiteApp">
+            <img src={bancking} alt="bancking" />
+            <button className="basketBtn">Добавить в корзину</button>
+            <h4>Элитный планировщик</h4>
+            <p>300 500 ₸</p>
+          </div>
+          <div className="definiteApp">
+            <img src={bancking} alt="bancking" />
+            <button className="basketBtn">Добавить в корзину</button>
+            <h4>Элитный планировщик</h4>
+            <p>300 500 ₸</p>
+          </div>
+          <div className="definiteApp">
+            <img src={bancking} alt="bancking" />
+            <button className="basketBtn">Добавить в корзину</button>
+            <h4>Элитный планировщик</h4>
+            <p>300 500 ₸</p>
+          </div>
+          <div className="definiteApp">
+            <img src={bancking} alt="bancking" />
+            <button className="basketBtn">Добавить в корзину</button>
+            <h4>Элитный планировщик</h4>
+            <p>300 500 ₸</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
